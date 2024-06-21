3680ddeda55dcaf2dfbce0cbb842cdbbcf8b296f
Tabby <me@t4bby.dev>
2024-06-22 03:50:06 +0800

    feat: remove useless packages

--- install.sh
+++ install.sh
@@ -1,52 +1,57 @@
 #!/bin/bash
 
-echo "[*] Installing Dependencies"
-cd /tmp
-sudo pacman -Sy git base-devel dkms
+echo "Vanilla Arch Linux Script for Steam Deck"
 
-# Some patches are in AUR
-echo "[*] Installing yay"
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-echo y | LANG=C makepkg -si
-
-echo "[*] Installing Jupiter Kernel Patches and Fan Control"
-echo y | LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" jupiter-dkms jupiter-fan-control
+if [[ $EUID -ne 0 ]]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit -1
+fi
 
 echo "[*] Installing SteamOS packages"
 
-# As much as posible, dont install generic packages from Steam Repository
+# But as much as posible, dont install generic packages from Steam Repository
 echo "[!] Adding jupiter repository to pacman.conf"
 if ! grep -q -F "[jupiter-main]" "/etc/pacman.conf"; then
-    sudo cat << EOF >> /etc/pacman.conf
+    cat << EOF >> /etc/pacman.conf
 
 # SteamOS Jupiter Repository
 [jupiter-main]
-Server = https://steamdeck-packages.steamos.cloud/archlinux-mirror/$repo/os/$arch
+Server = https://steamdeck-packages.steamos.cloud/archlinux-mirror/\$repo/os/\$arch
 SigLevel = Never
 
 EOF
 fi
 
 # Update all packages
-sudo pacman -Syu
+pacman -Syu
 
 echo "[-] Removing Dependecies Conflicts"
+
 # If you have amd-ucode installed, SteamOS already included this on their linux-firmware package
-sudo pacman -Rcns amd-ucode
+pacman -Rcns amd-ucode linux-firmware
 
 echo "[+] Installing SteamOS packages"
 
 # Install base SteamOS packages, cherrypick only the nessesary packages
-sudo pacman -Sy adobe-source-code-pro-fonts noto-fonts noto-fonts-cjk steamdeck-dsp gamemode gamescope jupiter-hw-support jupiter-legacy-support powerbuttond steam-jupiter-stable steam-im-modules upower vpower volume_key jupiter-hw-support linux-firmware-neptune linux-firmware-neptune-whence noise-suppression-for-voice
+pacman -Sy adobe-source-code-pro-fonts jupiter-main/noto-fonts \
+            jupiter-main/noto-fonts-cjk jupiter-main/steamdeck-dsp \
+            gamemode jupiter-main/gamescope jupiter-hw-support \
+            jupiter-legacy-support jupiter-main/powerbuttond \
+            steam-jupiter-stable steam-im-modules jupiter-main/upower \
+            jupiter-main/vpower jupiter-main/volume_key jupiter-hw-support \
+            linux-firmware-neptune linux-firmware-neptune-whence \
+            noise-suppression-for-voice
 
 echo "[!] Done installing packages!"
 
 echo "[+] Copying SteamOS rootfs"
 echo "[*] Changing permissions"
+
+# Make binary in rootfs executable
 chmod +x ./rootfs/usr/bin/*
 
-sudo cp ./rootfs/* /
+# Copy configurations
+cp -R ./rootfs/* /
 
 echo "[!] Done copying rootfs"
 echo "[+] You can now reboot to see the changes"
